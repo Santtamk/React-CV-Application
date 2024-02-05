a5e88c2df7190d19ea8d57f91e01a2ebf01cb893
Santam Kumai <santtamk@gmail.com>
2024-02-05 22:37:03 +0530

    final commit

--- react-cv-application/src/components/NameAndAddress.jsx
+++ react-cv-application/src/components/NameAndAddress.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "../styles/styles.css";
 
-const NameAndAddress = ({smallHeading}) => {
+const NameAndAddress = ({ smallHeading }) => {
   const [name, setName] = useState("");
   const [save, setSave] = useState(false);
+  const  [submittedName, setSubmittedName] = useState('')
 
   const cvName = (e) => {
     setName(e.target.value);
@@ -18,7 +19,7 @@ const NameAndAddress = ({smallHeading}) => {
       <h5>{smallHeading}</h5>
       <div className="nameAndAddress_subsection">
         <div>
-          {save ? <textarea type="text" onChange={cvName} /> : <p>{name}</p>}
+          {save ? <textarea type="text" onChange={cvName} value={name}/> : <p>{name}</p>}
         </div>
         <button onClick={saveAndEdit}>{save ? "Submit" : "Edit"}</button>
       </div>
